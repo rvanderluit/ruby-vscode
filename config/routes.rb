62c611e1ce5e483dbc46e7bda6e1af8f8ae6caf2
rvanderluit <rvanderluit@gmail.com>
2023-02-25 12:57:57 -0800

    gem simple formmm

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root "static_pages#landing_page"
+  get 'home/index'
+  root "home#index"
 
   get 'landing_page', to: "static_pages#landing_page"
   get 'privacy_policy', to: "static_pages#privacy_policy"
